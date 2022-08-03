b0aecf2b0b46cdbef6df4cee742c67db9d335526
fraanfe <fraanferrante11@gmail.com>
2022-08-03 11:14:57 -0300

    Create Ejercicio2py.py

---
+++ Ejercicio2py.py
@@ -0,0 +1,7 @@
+Nota1= entero = int( input("Ingresar Nota 1: "))
+Nota2= entero = int( input("Ingresar Nota 2: "))
+Nota3= entero = int( input("Ingresar Nota 3: "))
+
+Promedio = Nota1+Nota2+Nota3
+Final = Promedio / 3
+print ("El promedio del alumno es:", Final)
