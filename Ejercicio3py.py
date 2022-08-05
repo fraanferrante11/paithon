424166052128e835d879b5a1f1d2796cab3b03dd
fraanfe <fraanferrante11@gmail.com>
2022-08-05 09:19:41 -0300

    Update Ejercicio3py.py

--- Ejercicio3py.py
+++ Ejercicio3py.py
@@ -1,8 +1,10 @@
 from asyncore import read
 
 
-print ("Ingresar número de respuestas correctas")
-read (RC)
+RC= entero = int( input("Ingresar número de respuestas correctas: "))
+RI= entero = int( input("Ingresar número de respuestas incorrectas: "))
+RB= entero = int( input("Ingresar número de respuestas en blanco: "))
 
-
-
+PRC= RC*3
+PRI= RI*(-1)
+PRB= RB*0
